fa432f993713d25fd41f85482b5e43c485a0d62a
Bbypepe <babypepeofficially@gmail.com>
2024-10-16 09:24:24 +0300

    Update VoiceChanger.js

---
+++ smart-translation-app/src/components/VoiceChanger.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const VoiceChanger = () => {
+    return (
+        <div>
+            <h2>Voice Changer</h2>
+            {/* Add voice changing functionality here */}
+        </div>
+    );
+};
+
+export default VoiceChanger;

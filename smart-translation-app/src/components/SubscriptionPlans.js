1cda245e5ae9745a7cc49e5c52259920a8cb3bc0
Bbypepe <babypepeofficially@gmail.com>
2024-10-16 09:23:26 +0300

    Update SubscriptionPlans.js

---
+++ smart-translation-app/src/components/SubscriptionPlans.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SubscriptionPlans = () => {
+    return (
+        <div>
+            <h2>Subscription Plans</h2>
+            {/* List your subscription plans here */}
+        </div>
+    );
+};
+
+export default SubscriptionPlans;

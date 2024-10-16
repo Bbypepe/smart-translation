cc8bab056768233d7af7667252911f3551c19097
Bbypepe <babypepeofficially@gmail.com>
2024-10-16 06:26:40 +0300

    Create (Subscription management) SubscriptionPlans.js

---
+++ (Subscription management) SubscriptionPlans.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const SubscriptionPlans = () => {
+  return (
+    <div>
+      <h1>Subscription Plans</h1>
+      <ul>
+        <li>Text and Voice Translation: $25/month</li>
+        <li>Real-time Voice Change and Translation: $75/month</li>
+        <li>Viewer Message Translation: $25/month</li>
+        <li>All Services: $130/month</li>
+      </ul>
+      <button>Start Free Trial (3 days)</button>
+    </div>
+  );
+};
+
+export default SubscriptionPlans;
